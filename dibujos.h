8cb7eda3c9ef268745d04227b70cb1eefe974ab3
Cami-2901 <aguirrecamila2901@gmail.com>
2026-05-10 21:45:30 -0300

    Mis anteriores modificaciones con el main nuevo

--- dibujos.h
+++ dibujos.h
@@ -6,6 +6,14 @@
 
 #define PIXELES_X_LADO 8
 #define PX_PADDING 0
+#define ANCHO_DIGITO 3
+#define ALTO_DIGITO  5
+
+// Cada dígito es una matriz de 5 filas x 3 columnas
+// 1 = píxel encendido, 0 = transparente
+extern const uint8_t digitos[10][5][3];
+extern const uint8_t letras_score[5][5][3];
+extern const uint8_t letras_next[4][5][3];
 
 #define N 15 // Transparente
 #define A 14 // Amarillo
@@ -13,5 +21,5 @@
 #define V 2  // Verde
 
 void dibujar(const uint8_t sprite[][PIXELES_X_LADO], uint16_t oX, uint16_t oY);
-
+void dibujar_numero(int numero, uint16_t px, uint16_t py, uint8_t color);
 #endif
--- main.c
+++ main.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdint.h>
+#include <string.h>
 #include "dibujos.h"
 #include "GBT/gbt.h"
 
-#define ANCHO_VENTANA 80
+#define ANCHO_VENTANA 120
 #define ALTO_VENTANA 160
 #define ESCALA_VENTANA 5
 
@@ -29,7 +30,7 @@ const uint8_t bloque[7][8][8] = {
     {{A,A,A,A,A,A,A,A},{A,A,A,A,A,A,A,A},{A,A,A,A,A,A,A,A},{A,A,A,A,A,A,A,A},{A,A,A,A,A,A,A,A},{A,A,A,A,A,A,A,A},{A,A,A,A,A,A,A,A},{A,A,A,A,A,A,A,A}}
 };
 
-uint8_t piezas[CANT_PIEZAS][4][4] = {
+const uint8_t piezas_orig[CANT_PIEZAS][4][4] = {
     {{0,0,0,0},{1,1,1,1},{0,0,0,0},{0,0,0,0}},
     {{1,1,0,0},{1,1,0,0},{0,0,0,0},{0,0,0,0}},
     {{0,1,0,0},{1,1,1,0},{0,0,0,0},{0,0,0,0}},
@@ -39,13 +40,21 @@ uint8_t piezas[CANT_PIEZAS][4][4] = {
     {{0,0,1,0},{1,1,1,0},{0,0,0,0},{0,0,0,0}}
 };
 
-int pieza_actual, posX, posY;
+// Copia mutable que se modifica al rotar
+uint8_t piezas[CANT_PIEZAS][4][4];
 
+int game_over = 0;
+int puntaje = 0;
+int pieza_actual, posX, posY;
+int siguiente;
 int bag[7], bag_index=0;
 
 void mezclar_bag(){
     for(int i=0;i<7;i++) bag[i]=i;
-    for(int i=0;i<7;i++){int j=rand()%7;int t=bag[i];bag[i]=bag[j];bag[j]=t;}
+    for(int i=6;i>0;i--){      // <-- empieza desde el final
+        int j=rand()%(i+1);    // <-- rango se achica cada iteración
+        int t=bag[i];bag[i]=bag[j];bag[j]=t;
+    }
     bag_index=0;
 }
 
@@ -88,6 +97,7 @@ void fijar(){
 }
 
 void limpiar(){
+    int filas = 0;
     for(int y=0;y<ALTO_TABLERO;y++){
         int llena=1;
         for(int x=0;x<ANCHO_TABLERO;x++)
@@ -96,22 +106,34 @@ void limpiar(){
             for(int yy=y;yy>0;yy--)
                 for(int x=0;x<ANCHO_TABLERO;x++)
                     tablero[yy][x]=tablero[yy-1][x];
+            y--;
+            filas++;
         }
     }
+    int tabla[] = {0, 100, 300, 500, 800};
+    if(filas > 0) puntaje += tabla[filas];
 }
 
+
+
 void nueva(){
-    pieza_actual=siguiente_pieza();
-    posX=3; posY=0;
-    if(colisiona(piezas[pieza_actual],posX,posY)){
-        printf("GAME OVER\n");
-        exit(0);
+    pieza_actual = siguiente;
+    siguiente = siguiente_pieza();
+
+    memcpy(piezas[pieza_actual], piezas_orig[pieza_actual], sizeof(piezas[0]));
+
+    posX = 3;
+    posY = 0;
+
+    if(colisiona(piezas[pieza_actual], posX, posY)){
+        game_over = 1;
     }
 }
 
 void render(){
     gbt_borrar_backbuffer(0);
 
+    // Tablero y pieza actual (igual que antes)
     for(int y=FILAS_OCULTAS;y<ALTO_TABLERO;y++)
         for(int x=0;x<ANCHO_TABLERO;x++)
             if(tablero[y][x])
@@ -124,12 +146,58 @@ void render(){
                 if(dy>=0)
                     dibujar(bloque[pieza_actual],posX+j,dy);
             }
+
+// Dibujar "SCORE"
+    for(int k = 0; k < 5; k++)
+    for(int ly = 0; ly < 5; ly++)   // ly en lugar de y
+        for(int lx = 0; lx < 3; lx++)  // lx en lugar de x
+            if(letras_score[k][ly][lx])
+                gbt_dibujar_pixel(84 + k*(ANCHO_DIGITO+1) + lx, 2 + ly, A);  // 2 + ly
+
+// Puntaje debajo del texto (solo una vez)
+    dibujar_numero(puntaje, 84, 10, A);
+
+// Panel de puntaje (a la derecha, desde x=82)
+// Etiqueta "SC" en píxeles (simplificado, podés expandirlo)
+    dibujar_numero(puntaje, 84, 10, A); // color amarillo
+
+// Dibujar "NEXT"
+    for(int k = 0; k < 4; k++)
+    for(int ly = 0; ly < 5; ly++)
+        for(int lx = 0; lx < 3; lx++)
+            if(letras_next[k][ly][lx])
+                gbt_dibujar_pixel(84 + k*(ANCHO_DIGITO+1) + lx, 30 + ly, A);
+
+// Dibujar preview de siguiente pieza
+    for(int i=0;i<4;i++)
+    for(int j=0;j<4;j++)
+        if(piezas_orig[siguiente][i][j]){
+// posición fija en panel derecho
+        dibujar(bloque[siguiente], 11 + j, 6 + i);
+        }
+    int baseX = 0;
+    int baseY = 0;
+
+    int ancho_px = ANCHO_TABLERO * PIXELES_X_LADO;
+    int alto_px  = ALTO_VISIBLE * PIXELES_X_LADO;
+
+// Línea superior e inferior
+for(int x = 0; x < ancho_px; x++){
+    gbt_dibujar_pixel(baseX + x, baseY, V); // arriba
+    gbt_dibujar_pixel(baseX + x, baseY + alto_px - 1, V); // abajo
+}
+
+// Línea izquierda y derecha
+for(int y = 0; y < alto_px; y++){
+    gbt_dibujar_pixel(baseX, baseY + y, V); // izquierda
+    gbt_dibujar_pixel(baseX + ancho_px - 1, baseY + y, V); // derecha
+}
 }
 
 //Aceleracion
 void actualizar_dificultad(tGBT_Temporizador** timer, int* cont, float* intervalo) {
     (*cont)++;
-    if ((*cont) % 10 == 0) { //Cada 10 fichas
+    if ((*cont) % 10 == 0) { // El PDF pide cada 10 fichas
         (*intervalo) *= 0.97f;
 
         gbt_temporizador_destruir(*timer);
@@ -140,7 +208,9 @@ void actualizar_dificultad(tGBT_Temporizador** timer, int* cont, float* interval
 }
 
 int main(){
+
     srand(time(0));
+
     //Le agregue aca la "verificacion" de q se haya ejecutado bien, tanto al iniciar como cuando abre la ventana
     if (gbt_iniciar() != 0) {
         fprintf(stderr, "Error al iniciar GBT: %s\n", gbt_obtener_log());
@@ -152,40 +222,56 @@ int main(){
         return -1;
     }
 
+    memcpy(piezas, piezas_orig, sizeof(piezas_orig));  // ← primero
     mezclar_bag();
+    siguiente = siguiente_pieza(); // preload
     nueva();
+    tGBT_Temporizador* timer = gbt_temporizador_crear(0.5);
+    int delay_mov = 0;
 
-    tGBT_Temporizador* timer=gbt_temporizador_crear(0.5);
-
-    //variables
-    int contador_fichas = 0; // Cant de piezas q se han fijado
+    //variables --> aceleracion
+    int contador_fichas = 0;       // Cant de piezas q se han fijado
     float intervalo_actual = 1000;
-
-    while(1){
+    while(!game_over){
         gbt_procesar_entrada();
-        eGBT_Tecla t=gbt_obtener_tecla_presionada();
-
-        if(t==GBTK_ESCAPE) break;
-
-       if(t == GBTK_a){
-    posX--;
-    if(colisiona(piezas[pieza_actual], posX, posY)) posX++;
-}
-
-if(t == GBTK_d){
-    posX++;
-    if(colisiona(piezas[pieza_actual], posX, posY)) posX--;
-}
-
-if(t == GBTK_s){
-    posY++;
-    if(colisiona(piezas[pieza_actual], posX, posY)) posY--;
-}
-
-if(t == GBTK_w){
-    intentar_rotar();
-}
-
+        // IZQUIERDA
+        if(gbt_tecla_presionada(GBTK_IZQUIERDA)){
+            posX--;
+            if(colisiona(piezas[pieza_actual], posX, posY)) posX++;
+        }
+        // DERECHA
+        if(gbt_tecla_presionada(GBTK_DERECHA)){
+            posX++;
+            if(colisiona(piezas[pieza_actual], posX, posY)) posX--;
+        }
+        // ABAJO (1 paso por toque)
+        if(gbt_tecla_presionada(GBTK_ABAJO)){
+            posY++;
+            if(colisiona(piezas[pieza_actual], posX, posY)){
+                posY--;
+            } else {
+                puntaje += 1;
+            }
+        }
+        // ROTAR
+        if(gbt_tecla_presionada(GBTK_ARRIBA)){
+            intentar_rotar();
+        }
+        // HARD DROP (ESPACIO)
+        if(gbt_tecla_presionada(GBTK_ESPACIO)){
+            while(!colisiona(piezas[pieza_actual], posX, posY)){
+                posY++;
+                puntaje += 2;
+            }
+            posY--;
+            fijar();
+            limpiar();
+            nueva();
+        }
+        // SALIR
+        if(gbt_tecla_presionada(GBTK_ESCAPE)){
+            break;
+        }
         if(gbt_temporizador_consumir(timer)){
             posY++;
             if(colisiona(piezas[pieza_actual],posX,posY)){
@@ -202,10 +288,12 @@ if(t == GBTK_w){
         gbt_esperar(16);
     }
 
-    //Aqui, no estoy muy segura de esto, pero le agregue lo de limpieza de memoria
+    printf("GAME OVER\n");
+
+    //Aqui, no estoy muy segura de esto, pero le agregue lo de limpieza de memoria;
     gbt_temporizador_destruir(timer);
     gbt_destruir_ventana();
 
-    gbt_cerrar();
+    gbt_cerrar(); // ahora sí se llama correctamente
     return 0;
 }
